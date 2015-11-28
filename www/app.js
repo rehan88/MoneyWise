a4a696a70a5c920d82531be4dc17d46b177e252a
Rehan Zaheer <zaheer_rehan@hotmail.com>
2015-11-28 12:51:10 +0000

    seperated out route config from app.js

--- www/app.js
+++ www/app.js
@@ -1,76 +1,34 @@
-// (function () {
-  var db = null;
+var db = null;
   
-  angular.module('BudgetApp', ['ionic','ionic-material','ngCordova', 'app.services', 'starter.controllers', 'app.accounts', 'app.expenses', 'app.directdebits', 'app.income', 'ionic-toast'])
+angular.module('BudgetApp', ['ionic',
+                             'app.routeconfig',
+                             'ionic-material',
+                             'ngCordova', 
+                             'app.services', 
+                             'starter.controllers', 
+                             'app.accounts', 
+                             'app.expenses', 
+                             'app.directdebits', 
+                             'app.income', 
+                             'ionic-toast'])
     
-    .run(function ($ionicPlatform, $cordovaSQLite) {
-      $ionicPlatform.ready(function () {
+.run(function ($ionicPlatform, $cordovaSQLite) { $ionicPlatform.ready(function () {
 
         if (window.cordova && window.cordova.plugins.Keyboard) {
           cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
           cordova.plugins.Keyboard.disableScroll(true);
-        }
+        };
         if (window.StatusBar) {
           StatusBar.styleDefault();
-        }
-        
-         if(window.cordova) {
-          // App syntax
+        };        
+        if(window.cordova) {        
           db = $cordovaSQLite.openDB("MoneyWise.db");
-        } else {
-          // Ionic serve syntax
+        } 
+        else 
+        {          
           db = window.openDatabase("MoneyWise.db", "1.0", "Money Wise", -1);
-        }
-    
+        }    
         $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS accounts (id integer primary key, accountname text, balance text)");
         
       });
-    })
-    
-    .config(function ($stateProvider, $urlRouterProvider) {
-      $stateProvider
-        .state('app', {
-          url: '/app',
-          abstract: true,
-          templateUrl: 'menu/menu.html',
-          controller: 'menucontroller'
-        })
-        .state('app.accounts', {
-          url: '/accounts',
-          views: {
-            'menuContent': {
-              templateUrl: 'accounts/accounts.html', 
-              controller: 'accountscontroller'
-            }
-          }
-        })
-        .state('app.expenses', {
-          url: '/expenses/:accountid',
-          views: {
-            'menuContent': {
-              templateUrl: 'expenses/expenses.html',
-              controller : 'expensescontroler' 
-            }
-          }
-        })
-        .state('app.income', {
-          url: '/income',
-          views: {
-            'menuContent': {
-              templateUrl: 'income/income.html',
-              controller: 'incomecontroller'
-            }
-          }
-        })
-        .state('app.directdebits', {
-          url: '/directdebits',
-          views: {
-            'menuContent': {
-              templateUrl: 'directdebits/directdebits.html',
-              controller: 'directdebitscontroller'
-            }
-          }
-        });;
-      $urlRouterProvider.otherwise('/app/accounts');
-    });
-// })();
+ })    
---
+++ www/routeconfig.js
@@ -0,0 +1,51 @@
+ (function () {
+  
+  angular.module('app.routeconfig', ['ionic',])    
+         .config(function ($stateProvider, $urlRouterProvider) {
+          $stateProvider
+            .state('app', {
+              url: '/app',
+              abstract: true,
+              templateUrl: 'menu/menu.html',
+              controller: 'menucontroller'
+            })
+            .state('app.accounts', {
+              url: '/accounts',
+              views: {
+                'menuContent': {
+                  templateUrl: 'accounts/accounts.html', 
+                  controller: 'accountscontroller'
+                }
+              }
+            })
+            .state('app.expenses', {
+              url: '/expenses/:accountid',
+              views: {
+                'menuContent': {
+                  templateUrl: 'expenses/expenses.html',
+                  controller : 'expensescontroler' 
+                }
+              }
+            })
+            .state('app.income', {
+              url: '/income',
+              views: {
+                'menuContent': {
+                  templateUrl: 'income/income.html',
+                  controller: 'incomecontroller'
+                }
+              }
+            })
+            .state('app.directdebits', {
+              url: '/directdebits',
+              views: {
+                'menuContent': {
+                  templateUrl: 'directdebits/directdebits.html',
+                  controller: 'directdebitscontroller'
+                }
+              }
+            });;
+          $urlRouterProvider.otherwise('/app/accounts');
+        });
+
+})();
