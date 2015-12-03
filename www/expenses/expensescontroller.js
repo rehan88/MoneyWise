b966aaa93a85e7530ad7148508ffef9acb352f17
rehan88 <zaheer_rehan@hotmail.com>
2015-12-03 12:44:15 +0000

    added ability add expenses

--- www/expenses/expensescontroller.js
+++ www/expenses/expensescontroller.js
@@ -6,31 +6,38 @@
 
       function expensescontroler($scope, $stateParams, $location, Accounts, Expenses) {
 
+            $scope.expenses = [];
             $scope.expenseFormData = {};
             $scope.accountId = $stateParams.accountid;
             $scope.accountName = "";
-  
+
             $scope.populateAccount = function () {
                   Accounts.get($scope.accountId).then(function (account) {
                         $scope.accountName = account.accountname;
                   });
             };
-                        
+
+            $scope.populateAllExpenses = function () {
+                  Expenses.all().then(function (expenses) {
+                         $scope.expenses = expenses;                         
+                  });
+            };
+
             $scope.onSwipeRight = function (account) {
                   $location.url("app/accounts");
             };
 
             $scope.submitExpense = function () {
-                  $scope.newExpense = { 
-                        'item': $scope.expenseFormData.purchaseItem, 
-                        'amountspent': $scope.expenseFormData.amountSpent, 
-                        'datespent' : new Date().toLocaleDateString()
-                        };
-                  
-                  Expenses.add($scope.newExpense);      
-                  
+                  $scope.newExpense = {
+                        'item': $scope.expenseFormData.purchaseItem,
+                        'amountspent': $scope.expenseFormData.amountSpent,
+                        'datespent': new Date().toLocaleDateString()
+                  };
+                  Expenses.add($scope.newExpense);
+                  $scope.populateAllExpenses();
             };
-            
+
             $scope.populateAccount();
+            $scope.populateAllExpenses();
       };
 })();
