297bc34f2db832f67a42ae163631bf783a612560
unknown <rzaheer@LWLT338.lowell2.local>
2015-12-31 14:28:00 +0000

    added add modal to account

--- www/accounts/accountscontroller.js
+++ www/accounts/accountscontroller.js
@@ -8,43 +8,57 @@
 
         $scope.accounts = [];
         $scope.accountToDelete = {};
+        $scope.isNotValid = false;
 
         $scope.$on('$ionicView.enter', function () {
             $scope.populateAllAccounts();
         });
-        
+
         $scope.accountFormData = {
             "accountname": "",
             "balance": ""
         };
-        
-        $ionicModal.fromTemplateUrl('/accounts/deletemodal/delete-modal.html', {
+
+        $ionicModal.fromTemplateUrl('/accounts/addmodal/add-modal.html', {
+            id: 1,
             scope: $scope,
             animation: 'slide-in-up'
         }).then(function (modal) {
-            $scope.modal = modal;
+            $scope.oModal1 = modal;
         });
-        
+
+        $ionicModal.fromTemplateUrl('/accounts/deletemodal/delete-modal.html', {
+            id: 2,
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.oModal2 = modal;
+        });
+
+        $scope.openModal = function (index) {
+            if (index == 1) $scope.oModal1.show();
+            else $scope.oModal2.show();
+        };
+
+        $scope.closeModal = function (index) {
+            if (index == 1) $scope.oModal1.hide();
+            else $scope.oModal2.hide();
+        };
+
+
         $scope.onSwipeLeft = function (account) {
             $location.url("app/expenses/" + account.id);
         };
-        
+
         $scope.populateAllAccounts = function () {
-            Accounts.all().then(function (accounts) {                
-                $scope.accounts = accounts;                 
-                if(accounts.length === 0){
-                   $scope.noAccounts = true;
-                }               
+            Accounts.all().then(function (accounts) {
+                $scope.accounts = accounts;
+                if (accounts.length === 0) {
+                    $scope.noAccounts = true;
+                }
             });
         };
-        
-        $scope.showAddNewAccount = function () {
-            $scope.noAccounts = false;
-            $scope.addAccount = true;
-            $scope.hideAddNewAccountBtn = true;
-            $scope.focusInputAccountName = true;
-        };
-        
+
         $scope.showSubmitToast = function (accountName, toastType) {
             if (toastType === "success") {
                 ionicToast.show("The account " + accountName + ' account has been added.', 'bottom', false, 2500);
@@ -53,50 +67,52 @@
                 ionicToast.show("The account " + accountName + ' has been removed.', 'bottom', false, 2500);
             }
         };
-        
-        $scope.showRequiredToast = function (field) {
-            ionicToast.show('Please enter' + field, 'bottom', false, 2500);
+
+        $scope.showRequiredToast = function () {
+            ionicToast.show('All fields are required', 'bottom', false, 3000);
         };
-        
+
         $scope.hideNewAccount = function () {
             $scope.focusInputAccountName = false;
-            $scope.addAccount = false;
-            $scope.hideAddNewAccountBtn = false;
         };
-                
+
         $scope.setAccountToDelete = function (account) {
             $scope.accountToDelete = account;
         };
-        
+
         $scope.deleteAccount = function () {
             Accounts.remove($scope.accountToDelete);
             Expenses.removeForAccount($scope.accountToDelete);
             $scope.populateAllAccounts();
             $scope.showSubmitToast($scope.accountToDelete.accountname, "remove");
         };
-        
-        $scope.isEmpty = function () {
-            if ($scope.accountFormData.accountname === undefined || $scope.accountFormData.accountname === "") {
-                $scope.showRequiredToast(" an account name");
-                return true;
-            }
-            if ($scope.accountFormData.balance === undefined || $scope.accountFormData.balance === "" || $scope.accountFormData.balance === null) {
-                $scope.showRequiredToast(" a balance");
-                return true;
-            }
+
+        $scope.checkFields = function (arrayToCheck) {
+            angular.forEach(arrayToCheck, function (value, key) {
+                if (value === undefined || value === "" || value === null) {
+                    $scope.showRequiredToast();
+                    $scope.isNotValid = true;
+                } else {
+                    $scope.isNotValid = false;
+                }
+            });
         };
-        
+
+        $scope.cleanForm = function () {
+            $scope.accountFormData.accountname = '';
+            $scope.accountFormData.balance = '';
+        };
+
         $scope.submitNewAccount = function () {
-            if ($scope.isEmpty()) {
+            $scope.checkFields($scope.accountFormData);
+            if ($scope.isNotValid) {
                 return false;
             };
             Accounts.add($scope.accountFormData);
             $scope.populateAllAccounts();
-            $scope.addAccount = false;
-            $scope.hideAddNewAccountBtn = false;
+            $scope.oModal1.hide()
             $scope.showSubmitToast($scope.accountFormData.accountname, "success");
-            $scope.accountFormData.accountname = '';
-            $scope.accountFormData.balance = '';
+            $scope.cleanForm();
         };
     }
 })();
